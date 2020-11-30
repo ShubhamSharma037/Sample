4fe6d8b2f3320da839eff95f7c0c2ee2251f03ea
Shubham Sharma <Shubhamsharma616.ss@gmail.com>
2020-12-01 00:53:31 +0530

    Updates

---
+++ precache-manifest.1fb0c0ea0267c1be46458960d1b89477.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d706d5afeed627c67d40e9bc0930a63d",
+    "url": "/Sample/index.html"
+  },
+  {
+    "revision": "bace418c9e90e3fe1825",
+    "url": "/Sample/static/css/2.6095e193.chunk.css"
+  },
+  {
+    "revision": "97ec6167c318646d4c6b",
+    "url": "/Sample/static/css/main.63eb681e.chunk.css"
+  },
+  {
+    "revision": "bace418c9e90e3fe1825",
+    "url": "/Sample/static/js/2.06ad1c81.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Sample/static/js/2.06ad1c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ec6167c318646d4c6b",
+    "url": "/Sample/static/js/main.378ae424.chunk.js"
+  },
+  {
+    "revision": "3446f48cd7195351b66c",
+    "url": "/Sample/static/js/runtime-main.bdf711af.js"
+  },
+  {
+    "revision": "73656f4ac61d2170bf1b91b1a8cd1b39",
+    "url": "/Sample/static/media/arc.73656f4a.svg"
+  },
+  {
+    "revision": "6adbc93c7f5fe23cdf81177315079400",
+    "url": "/Sample/static/media/bg_1.6adbc93c.jpg"
+  },
+  {
+    "revision": "97d65cced8075fb32114cfed636fc9b8",
+    "url": "/Sample/static/media/counter_bg.97d65cce.webp"
+  },
+  {
+    "revision": "5c2f3d34be433938ae44c502f7bb01df",
+    "url": "/Sample/static/media/home1.5c2f3d34.jpg"
+  },
+  {
+    "revision": "e5077bad411730b3f697ad6b0b2476e9",
+    "url": "/Sample/static/media/home2.e5077bad.jpg"
+  },
+  {
+    "revision": "12cab8ae1974a824c424623002a230bf",
+    "url": "/Sample/static/media/home3.12cab8ae.jpg"
+  },
+  {
+    "revision": "954af54b04567bc648f33130901ecc6a",
+    "url": "/Sample/static/media/home4.954af54b.jpg"
+  },
+  {
+    "revision": "daee0ca9071e45e82bd5343e6b95d1de",
+    "url": "/Sample/static/media/mail.daee0ca9.webp"
+  },
+  {
+    "revision": "c2fd9a34baaded01ec265a9fb08fdf47",
+    "url": "/Sample/static/media/pen.c2fd9a34.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Sample/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Sample/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Sample/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "164e8d7d32a6749d917a550f48c80621",
+    "url": "/Sample/static/media/service1.164e8d7d.jpg"
+  },
+  {
+    "revision": "8d5cf7c515c3bb93c375d3c74bc3657f",
+    "url": "/Sample/static/media/service2.8d5cf7c5.jpg"
+  },
+  {
+    "revision": "f58fe1eacd91b86c6eef1ed800190588",
+    "url": "/Sample/static/media/service3.f58fe1ea.jpg"
+  },
+  {
+    "revision": "b712e6ca800580512c270000397d1165",
+    "url": "/Sample/static/media/tools.b712e6ca.svg"
+  },
+  {
+    "revision": "dbb6c9dedfeeed39460dba9d9ee0af3f",
+    "url": "/Sample/static/media/workflow1.dbb6c9de.png"
+  },
+  {
+    "revision": "3a02eedf7ede4115c6dbf87abb6e73fb",
+    "url": "/Sample/static/media/workflow2.3a02eedf.webp"
+  }
+]);
