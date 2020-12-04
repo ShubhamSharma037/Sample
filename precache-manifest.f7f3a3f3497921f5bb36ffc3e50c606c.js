25601265a562af1132be62a0043c368733f479e4
Shubham Sharma <Shubhamsharma616.ss@gmail.com>
2020-12-04 22:48:25 +0530

    Updates

--- precache-manifest.f7f3a3f3497921f5bb36ffc3e50c606c.js
+++ precache-manifest.f7f3a3f3497921f5bb36ffc3e50c606c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "febf76be106953fbf022f82d172b0a0d",
+    "revision": "d0c1e6443f35aa4cd60ae87f7f4dd36c",
     "url": "/Sample/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Sample/static/css/2.6095e193.chunk.css"
   },
   {
-    "revision": "aaaa7d7ab2b8d0879a55",
-    "url": "/Sample/static/css/main.b1bbc483.chunk.css"
+    "revision": "a65a63cdf7ecbd20a40c",
+    "url": "/Sample/static/css/main.c4cfe26f.chunk.css"
   },
   {
     "revision": "a4cbb1dc6697b979d567",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Sample/static/js/2.cd1bf4a0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aaaa7d7ab2b8d0879a55",
-    "url": "/Sample/static/js/main.f71347fc.chunk.js"
+    "revision": "a65a63cdf7ecbd20a40c",
+    "url": "/Sample/static/js/main.e333f5cb.chunk.js"
   },
   {
     "revision": "3446f48cd7195351b66c",
