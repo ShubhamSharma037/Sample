1fdce761bad5e98a21c07b705707bd382fd6fe11
Shubham Sharma <Shubhamsharma616.ss@gmail.com>
2020-12-05 13:57:40 +0530

    Final Commit For Responsive

--- src/Components/Contact/ContactDetails/Detail/Detail.tsx
+++ src/Components/Contact/ContactDetails/Detail/Detail.tsx
@@ -11,10 +11,12 @@ type Props ={
 const Email : React.FC<Props> = (props) =>{
     return(
         <div className={classes.detail}>
-           <img src={props.image} alt='dpdzines'/>
-           <h1>
-               <a target='_blank' rel='noopener noreferrer' href={props.email?`mailto:${props.children}`:(props.link?`${props.children}`:`tel:${props.children}`)}>{props.children}</a>
-           </h1>
+            <div className={classes.detailContent}>
+                <img src={props.image} alt='dpdzines'/>
+                <h1>
+                    <a target='_blank' rel='noopener noreferrer' href={props.email?`mailto:${props.children}`:(props.link?`${props.children}`:`tel:${props.children}`)}>{props.children}</a>
+                </h1>
+            </div>
         </div>
     )
 }
