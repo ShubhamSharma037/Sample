a64e8382c61a44a6ebef0f8d221788fad8bfae5a
Shubham Sharma <Shubhamsharma616.ss@gmail.com>
2022-02-24 00:36:50 +0530

    Project Fixed

--- src/Components/Project/ProjectContainer/ProjectContainer.tsx
+++ src/Components/Project/ProjectContainer/ProjectContainer.tsx
@@ -11,13 +11,10 @@ const projectNames = [
      'Gurgaon, Deepak Ahlawat',
      'Gurgaon, Dr. Rakesh Chopra',
      'Homes 121, Noida, Mr. Manoj Chaudhary',
-     'Lajpat Nagar, Delhi, Arun Gupta',    //
      'Loni, Vishal Nagar',
      'Mr Suresh, Ats Advantage',
      'Mr Welly',
-     'Mr. Rajeev Arora',                   //
      'Mrs Anita (Gurgaon)',
-     'Noida 137, Amit Yadav',               //
      'Palm Olympia, Arvind Mital',
      'Pavillion Court Noida',
      'Sarita Vihar, Manoj Basoya',
@@ -28,20 +25,20 @@ const projectNames = [
 const pr1 : {[key : string] : string} =  importAllImages(require.context('../../../media/Projects/Gurgaon, Deepak Ahlawat',false,/\.(png|jpe?g|svg)$/));
 const pr2 : {[key : string] : string} =  importAllImages(require.context('../../../media/Projects/Gurgaon, Dr. Rakesh Chopra',false,/\.(png|jpe?g|svg)$/));
 const pr3 : {[key : string] : string} =  importAllImages(require.context('../../../media/Projects/Homes 121, Noida, Mr. Manoj Chaudhary',false,/\.(png|jpe?g|svg)$/));
-const pr4 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Lajpat Nagar, Delhi, Arun Gupta',false,/\.(png|jpe?g|svg)$/));
-const pr5 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Loni, Vishal Nagar',false,/\.(png|jpe?g|svg)$/));
-const pr6 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Mr Suresh, Ats Advantage',false,/\.(png|jpe?g|svg)$/));
-const pr7 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Mr Welly',false,/\.(png|jpe?g|svg)$/));
-const pr8 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Mr. Rajeev Arora',false,/\.(png|jpe?g|svg)$/));
-const pr9 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Mrs Anita (Gurgaon)',false,/\.(png|jpe?g|svg)$/));
-const pr10 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Noida 137, Amit Yadav',false,/\.(png|jpe?g|svg)$/));
-const pr11 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Palm Olympia, Arvind Mital',false,/\.(png|jpe?g|svg)$/));
-const pr12 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Pavillion Court Noida',false,/\.(png|jpe?g|svg)$/));
-const pr13 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Sarita Vihar, Manoj Basoya',false,/\.(png|jpe?g|svg)$/));
-const pr14 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Saya Zenith, Dr. Neha',false,/\.(png|jpe?g|svg)$/));
-const pr15 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Supertech Estate, Amit Aggarwal',false,/\.(png|jpe?g|svg)$/));
 
-const projectImages = [pr1,pr2,pr3,pr4,pr5,pr6,pr7,pr8,pr9,pr10,pr11,pr12,pr13,pr14,pr15];
+const pr4 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Loni, Vishal Nagar',false,/\.(png|jpe?g|svg)$/));
+const pr5 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Mr Suresh, Ats Advantage',false,/\.(png|jpe?g|svg)$/));
+const pr6 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Mr Welly',false,/\.(png|jpe?g|svg)$/));
+
+const pr7 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Mrs Anita (Gurgaon)',false,/\.(png|jpe?g|svg)$/));
+
+const pr8 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Palm Olympia, Arvind Mital',false,/\.(png|jpe?g|svg)$/));
+const pr9 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Pavillion Court Noida',false,/\.(png|jpe?g|svg)$/));
+const pr10 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Sarita Vihar, Manoj Basoya',false,/\.(png|jpe?g|svg)$/));
+const pr11 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Saya Zenith, Dr. Neha',false,/\.(png|jpe?g|svg)$/));
+const pr12 : {[key : string] : string}=  importAllImages(require.context('../../../media/Projects/Supertech Estate, Amit Aggarwal',false,/\.(png|jpe?g|svg)$/));
+
+const projectImages = [pr1,pr2,pr3,pr4,pr5,pr6,pr7,pr8,pr9,pr10,pr11,pr12];
 
 
 
