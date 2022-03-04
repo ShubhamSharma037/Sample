fb0dba738ce4285219b92ec8174854de70ea94da
Shubham Sharma <Shubhamsharma616.ss@gmail.com>
2022-03-04 17:26:55 +0530

    Counter Updated

--- src/Components/Home/Counter/content/content.tsx
+++ src/Components/Home/Counter/content/content.tsx
@@ -8,8 +8,8 @@ const content : React.FC = () =>{
     return(
         <div className={classes.content}>
             <Item end='8'>Years of Experience</Item>
-            <Item end='50' suffix='+'>Happy Clients</Item>
-            <Item end='64' suffix='+'>Finished Projects</Item>
+            <Item end='100' suffix='+'>Happy Clients</Item>
+            <Item end='100' suffix='+'>Finished Projects</Item>
             {/* <Item end='64' suffix='+'>Finished Projects</Item> */}
         </div>
     )
